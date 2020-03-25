59d769fac6be2b11eacb511282250349300dc0c8
Sarah <sarah.boening@web.de>
2020-03-25 09:49:17 +0100

    whatever

--- Embeddings/cbow.py
+++ Embeddings/cbow.py
@@ -72,13 +72,15 @@ def load_text(path, tokenizer):
 
 torch.manual_seed(1)
 
+# CHANGE GPU ID 
+
 # path = "G:\\MASTER\\raw_files\\Java\\small\\train\\"
-path = "/home/nilo4793/raid/corpora/Java/small/train/"
-outpath = "/home/nilo4793/raid/output/embedding/javasmall/"
+path = "/home/nilo4793/raid/corpora/AST/small/train/"
+outpath = "/home/nilo4793/raid/output/embedding/ast_small/"
 #outpath = "G:\\MASTER\\outputs\\embeddings\\"
 
 #vocab_path = "vocab_small.txt"
-vocab_path = "/home/nilo4793/raid/corpora/Java/small/vocab_nltk.txt"
+vocab_path = "/home/nilo4793/raid/corpora/AST/small/vocab.txt"
 
 CONTEXT_SIZE = 2  # 2 words to the left, 2 to the right
 tokenizer = tok.Tokenizer(vocab_path, "java")
@@ -88,6 +90,7 @@ raw_text = list(chain.from_iterable(raw_text))
 
 # By deriving a set from `raw_text`, we deduplicate the array
 vocab_size = tokenizer.get_vocab_len()
+print(vocab_size)
 print("building context vectors")
 data = []
 for i in range(2, len(raw_text) - 2):
@@ -101,10 +104,10 @@ print(data[:5])
 
 # loss model optimizer
 losses = []
-loss_function = nn.NLLLoss()
+loss_function = nn.CrossEntropyLoss()
 model = CBOW(vocab_size, embedding_dim=64)
 optimizer = optim.SGD(model.parameters(), lr=0.001)
-device = torch.device('cuda:7' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:6' if torch.cuda.is_available() else 'cpu')
 model = model.to(device)
 print("starting training")
 # 10 epoch
@@ -123,11 +126,13 @@ for epoch in range(epochs):
         model.train()
         context_idxs = [tokenizer.convert_tokens_to_ids(w) for w in context]
         target_idx = tokenizer.convert_tokens_to_ids(target)
+        print(target_idx)
         context_var = Variable(torch.LongTensor(context_idxs)).to(device)
         target_var = Variable(torch.LongTensor([target_idx])).to(device)
         model.zero_grad()
         log_probs = model(context_var)
-        winner = tokenizer._convert_id_to_token(torch.argmax(log_probs[0]).item())
+        print(log_probs)
+        #winner = tokenizer._convert_id_to_token(torch.argmax(log_probs[0]).item())
         loss = loss_function(log_probs, target_var)
         loss.backward()
         optimizer.step()
