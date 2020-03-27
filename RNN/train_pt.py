48966bab55cec4851773c7b65d6001787c58f282
Sarah B <38113904+SarahBoening@users.noreply.github.com>
2020-03-27 14:40:41 +0100

    Update train_pt.py

--- RNN/train_pt.py
+++ RNN/train_pt.py
@@ -45,7 +45,7 @@ def get_data_from_file(train_file, batch_size, seq_size, tokenizer):
         if path.startswith("tokenized_"):
             with(open(path, "r", encoding="utf-8", errors="replace")) as f:
                 print('loading tokenized file: ', path)
-                text = f.read()
+                text = f.read().split()
                 list.append(text)
         elif not path.startswith('cached') and path.endswith(".raw") and not os.path.isfile("tokenized_" + path):
             print('loading and tokenizing file: ', path)
